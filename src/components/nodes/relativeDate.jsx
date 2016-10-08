c9e95c51f0f5ee857da41759762c356a508b15c1
Alisson Patricio <eu@alisson.net>
2016-10-07 22:17:38 -0300

    Improve tooltip on RelativeDate

--- src/components/nodes/relativeDate.jsx
+++ src/components/nodes/relativeDate.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import {
     injectIntl,
     FormattedRelative,
 } from 'react-intl';
 
-const FormatedDate = injectIntl(({date, intl}) => (
-    <span title={intl.formatDate(date)}>
-        <FormattedRelative value={date}/>
-    </span>
-));
+const MyFormattedRelative = injectIntl(({date, intl}) => {
+	var tooltip = (<Tooltip id={`tip_rdate_${date}`}>{intl.formatDate(date)} às {intl.formatTime(date)}</Tooltip>);
+	return (<OverlayTrigger placement="top" overlay={tooltip}>
+        <span><FormattedRelative value={date} /></span>
+    </OverlayTrigger>);
+});
 
 export default class RelativeDate extends React.Component {
   render() {
-  	return <FormatedDate date={this.props.date} />
+  	return <MyFormattedRelative date={this.props.date} />
   }
 }
